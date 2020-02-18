b2bc4a742605a6334a820e272c04504f38688962
hadilfriaa <hadil.friaa@ynov.com>
2020-02-18 14:13:35 +0100

    update

--- Y-plateform/src/Controller/GamesController.php
+++ Y-plateform/src/Controller/GamesController.php
@@ -81,7 +81,7 @@ class GamesController extends AbstractController
 
 
     /**
-     * @Route("/games", name="games")
+     * @Route("/library", name="library")
      * voir tout les jeux 
      */
     public function games()
