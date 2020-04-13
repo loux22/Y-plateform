c3c6fb4c0c401c2695f79b011a332344a4ee2564
Eléa Carton <eleacarton@MacBook-de-Elea.local>
2020-04-13 12:49:49 +0200

    Design /profil/id

--- Y-plateform/src/Controller/UserController.php
+++ Y-plateform/src/Controller/UserController.php
@@ -224,39 +224,123 @@ class UserController extends AbstractController
      */
     public function profilUser(Request $request, $id) {
         $navbar = true;
+        // affichage des donnes du user connecté
+
+        // redirige si pas connecté
         $repo = $this -> getDoctrine() -> getRepository(User::class);
         $user = $repo -> find($id);
-        
-        if($user == null) {
-            return $this->redirectToRoute('profil');
-        }
 
+        $avatarUser = $user -> getAvatar();
+        if($user === null){
+            return $this->redirectToRoute('login');
+        }
         $u = $user->getAge();
         $stringValue = $u->format('Y-m-d H:i:s');
         $datetime1 = new \DateTime(); // date actuelle
         $datetime2 = new \DateTime($stringValue);
         $age = $datetime1->diff($datetime2, true)->y; // le y = nombre d'années ex : 22
-
+        // recuperer dernier avatar de l'user connecté, a deplacer dans modifier
+        $lastAvatar = $user -> getAvatar();
+        ////////////////////////////////////////
         $repository = $this-> getDoctrine() -> getRepository(Member::class);
         $member = $repository -> getUserProfil($user);
 
+        //jeux du joueur
         $repository = $this-> getDoctrine() -> getRepository(Game::class);
         $game = $repository -> getGameList($user);
 
+        //note des jeux
         $repository = $this-> getDoctrine() -> getRepository(Note::class);
         $note = $repository -> noteJ($id);
 
+        // modifier le profil
+        $form = $this -> createForm(UserModifyType::class, $user);
+        $form -> handleRequest($request);
+
+        $memberForm = $member[0];
+
+        $formM = $this -> createForm(MemberModifyType::class, $memberForm);
+        $formM -> handleRequest($request);
+        
+        $userNewPassword = $request->request->all();
+
+
+        if($form -> isSubmitted() && $form -> isValid()){
+            if($user -> getAvatar()){
+                if($user -> getAvatar() -> getClientOriginalName() != '0.png'){
+                    $user -> removeFile();
+                    unlink($this->getParameter('upload_avatar') . $lastAvatar);
+                }
+                
+                $file = $user->getAvatar();
+                $filename = 'fichier_' . time() . '_' . rand(1,99999) . '_' . md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move($this->getParameter('upload_avatar'), $filename);
+                $user-> setAvatar($filename);
+            } else{
+                $user -> setAvatar($avatarUser);
+            }
+            
+            $manager = $this-> getDoctrine() -> getManager();
+            $manager -> persist($user); //commit(git)
+            $manager -> flush(); // push(git)
+            $this -> addFlash('success','modification');
+        }
+
+        if($formM -> isSubmitted() && $formM -> isValid()){
+            $manager = $this-> getDoctrine() -> getManager();
+            $manager -> persist($memberForm); //commit(git)
+            $manager -> flush(); // push(git)
+            $this -> addFlash('success','modification');
+            
+        }
+
+        //Changer mot de passe
+        $userNewPassword = $request->request->all();
+        $actuelPassword = $user->getPassword();
+        if(isset($userNewPassword['lastPassword'])){
+            $userLastPassword = $userNewPassword['lastPassword'];
+            $NewPassword = $userNewPassword['newPassword'];
+            $userRptNewPassword = $userNewPassword['repeatNewPassword'];
+            if (password_verify($userLastPassword, $actuelPassword)) {
+                    if($NewPassword == $userRptNewPassword) {
+
+                        if(strlen($NewPassword) >= 8) {
+                            $newUser = new User; 
+                            $password = $passwordEncoder->encodePassword($newUser, $NewPassword);
+                            $user -> setPassword($password);
+                            $manager = $this-> getDoctrine() -> getManager();
+                            $manager -> persist($user); //commit(git)
+                            $manager -> flush(); // push(git)
+                            $this -> addFlash('success','Le mot de passe a été modifié !');
+                        }else {
+                            $this -> addFlash('errors','Le mot de passe doit faire minimum 8 caractères');
+                        }
+                        
+
+        
+                    } else {
+                        $this -> addFlash('errors','Les deux mots de passe ne sont pas identiques');
+                    }
+
+
+            } else {
+                $this -> addFlash('errors','Le mot de passse ne corresponds pas a celui actuel');
+            }
+            
+        }
         
 
-        return $this->render('user/profil_user.html.twig', [
+        return $this->render('user/profil.html.twig', [
+            'form' => $form -> createView(),
+            'formM' => $formM -> createView(),
             'user' => $user,
+            'actuelPassword' => $actuelPassword,
             'member' => $member,
             'age' => $age,
             'game' => $game,
             'note' => $note,
-            'navbar' => $navbar,
-            'dashboard' => 0
-        ]);
+            'navbar' => $navbar
+            ]);
     }
 
 
